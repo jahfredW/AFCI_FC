ea79cca7391e2a149f1f3cd464e9e2e44cc6ad9f
DamienPalm <palm.damien@icloud.com>
2022-12-02 11:47:28 +0100

    Damien/classe player update/ClasseJoueur.php

---
+++ Classes/ClasseJoueur.php
@@ -0,0 +1,14 @@
+<?php
+
+class Player {
+    private string $nom;
+    private string $prenom;
+    private int $age;
+    private string $pied;
+    private int $endurance;
+    private string $position;
+    private int $att;
+    private int $def;
+    private int $vitesse;
+    private int $id;
+}
